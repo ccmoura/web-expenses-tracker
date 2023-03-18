9216759f896491c3734ee363f1b1250286f071f9
Christopher Moura <christophermoura48@gmail.com>
2023-03-17 23:06:41 -0300

    creating subfolders for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  const expenses = [{title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}, {title: 'Car Insurance 2', amount: 294.67, date: new Date(2021, 2, 25)}]
+const App = () => {
+  const expenses = [{
+    title: 'Car Insurance', 
+    amount: 294.67, 
+    date: new Date(2021, 2, 28)
+  }, {
+    title: 'Car Insurance 2', 
+    amount: 294.67, 
+    date: new Date(2021, 2, 25)
+  }]
   
   return (
     <div>
