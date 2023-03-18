3a2c167ff549b42607c2dfb57c517c7989dd17cd
Christopher Moura <christophermoura48@gmail.com>
2023-03-17 22:20:16 -0300

    feat: creating new components for expenses and expense date

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [{title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)}, {title: 'Car Insurance 2', amount: 294.67, date: new Date(2021, 2, 25)}]
+  
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map(expense => (
-        <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date.toISOString()}/>
-        ))}
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
